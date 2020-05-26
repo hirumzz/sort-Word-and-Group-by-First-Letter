ae1c384b74296562b5a3cb86950a0eba9f6b541a
hirumzz <hirumzz.z@gmail.com>
2020-05-26 14:37:59 +0700

    Create sortWord.sh

---
+++ sortWord.sh
@@ -0,0 +1,28 @@
+if [[ "$1" != "" ]]; then
+        sed 's/\([0-9]\)/;\1/' $1 | sort -n -t\; -k2,2 | tr -d ';' >> .list
+        IFS=$'\r\n' GLOBIGNORE='*' command eval  'word=($(cat .list))'
+        countArray=${#word[@]}
+        a="0"
+        for (( c = 0; c<$countArray; c++ ))
+        do
+                b="$(echo ${word[$c]} | head -c 1)"
+                if [ "$a" == "$b" ]
+                then
+                        echo ${word[$c]}
+                else
+                        a=$b
+                        echo -e
+                        echo -e
+                        echo "---${a^}---"
+                        echo ${word[$c]}
+                fi;
+        done
+        echo -e
+        rm -r .list
+else
+        echo ""
+        echo "what file? please type name off file"
+        echo ""
+        echo "Example: "
+        echo ""
+        echo "sortWord.sh example.txt"                                                                                                                                          echo ""                                                                                                                                                         fi;
